2f3c4f6413fc6300124186596a21d4720b9e7215
vleunti@gmail.com <M@h@gon5626256>
2019-12-04 20:47:32 +0100

    hz

---
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+        System.out.println("Some message to say...1234ssrrttaa");
+        return "index";
+    }
+
+}
